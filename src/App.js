e3da2f4c7501b9be6ab9260e26b7c84d0c99501e
Ria Narang <rianarang6@gmail.com>
2021-09-08 22:25:05 -0400

    starting point

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Ria Narang</h1>
+        <h2>Welcome to my personal website</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am a 2A Mechatronics Engineering Student in the University of
+          Waterloo. I am passionate about problem solving, automation and
+          learning!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
